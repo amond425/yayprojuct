75a39f51bc4d82474de4046b300fa40b4540603e
amond425 <69014417+amond425@users.noreply.github.com>
2021-12-16 18:43:55 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name = "Some";
+var last_name = "Lad";
+
+console.log(first_name.concat(last_name));
+
+var numba1 = "2";
+var numba2 = "3";
+var math = numba1 + numba2 ;
+console.log(math);
